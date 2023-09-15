d8e04eb494ff9286de9637770d546028d6e93583
Eduardo Guedes Pereira Silva <eduardoguedespsn@gmail.com>
2023-09-15 00:24:06 -0300

    x

--- aplication/lib/main.dart
+++ aplication/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,19 +23,22 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:
-          const Color.fromRGBO(224, 223, 224, 1.0), // Cor de fundo da tela
+      backgroundColor: const Color.fromRGBO(224, 223, 224, 1.0),
       body: Center(
         child: Container(
           width: 530,
           height: 690,
           decoration: BoxDecoration(
-            color: Colors.white, // Cor de fundo do Container
+            color: Colors.white,
             border: Border.all(
-              color: const Color.fromRGBO(224, 223, 224, 1.0), // Cor da borda
-              width: 2.0, // Largura da borda (pode ajustar conforme desejado)
+              color: const Color.fromRGBO(224, 223, 224, 1.0),
+              width: 2.0,
             ),
-            borderRadius: BorderRadius.zero, // Define bordas retas
+            borderRadius: BorderRadius.zero,
+            image: DecorationImage(
+              image: AssetImage('assets/wwwroot/fundo.jpg'),
+              fit: BoxFit.cover,
+            ),
           ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -61,7 +66,10 @@ class MyHomePage extends StatelessWidget {
                   children: <Widget>[
                     ElevatedButton(
                       onPressed: () {
-                        // Adicione a ação desejada aqui
+                        // Navegar para a outra página em branco
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => FormPage(),
+                        ));
                       },
                       style: ElevatedButton.styleFrom(
                         primary: const Color.fromRGBO(150, 119, 255, 1.0),
@@ -79,10 +87,112 @@ class MyHomePage extends StatelessWidget {
                       ),
                     ),
                     const SizedBox(height: 50),
-                    const Text(
-                      'You have an account? Log-in',
+                    Text(
+                      'You have an account? ',
                       style: TextStyle(
                         fontSize: 17,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      'Log-in',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FormPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(224, 223, 224, 1.0),
+      body: Center(
+        child: Container(
+          width: 530,
+          height: 690,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: const Color.fromRGBO(224, 223, 224, 1.0),
+              width: 2.0,
+            ),
+            borderRadius: BorderRadius.zero,
+            image: DecorationImage(
+              image: AssetImage('assets/wwwroot/fundo.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Center(
+                  child: Align(
+                    alignment: const Alignment(0, 0.5),
+                    child: Text(
+                      'Spiice',
+                      style: TextStyle(
+                        fontSize: 65,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        // Navegar para a outra página em branco
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => FormPage(),
+                        ));
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: const Color.fromRGBO(150, 119, 255, 1.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        minimumSize: const Size(300, 60),
+                      ),
+                      child: const Text(
+                        'Sign-up',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                    Text(
+                      'You have an account? ',
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      'Log-in',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
                         color: Colors.black,
                       ),
                     ),
